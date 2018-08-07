9005675e7eb52d5848c69b855df8bf3f79fbfebe
crautou <crautou@motion-twin.fr>
2018-08-07 13:11:22 +0200

    Added BeholderPit example

---
+++ structure/simple/BeholderPit.hx
@@ -0,0 +1,24 @@
+var exitInBranch;
+function buildMainRooms(){
+    exitInBranch = Random.isBelow(0.7);
+    Struct.createRoomWithType("Entrance").setName("start")
+        .chain(Struct.createSpecificRoom("BossBeholder")).setName("end")
+        .chain(Struct.createSpecificExit("T_AncientTemple", "BossBeholderExit"))
+        .chain(Struct.createExit("T_Cemetery"));
+
+}
+
+function addTeleports(){
+}
+
+function setLevelProps(_levelProps){
+    setLevelPropsFrom("BeholderPit", _levelProps);
+}
+
+function setLevelInfo(_levelInfo){
+    setLevelInfoFrom("BeholderPit", _levelInfo);
+}
+
+function buildMobRoster(_mobList){
+    addMobRosterFrom("BeholderPit", _mobList);
+}
