c2c26e7546ff888238ac2f19d6d3992c036fd095
crautou <crautou@motion-twin.fr>
2018-08-10 16:08:37 +0200

    BoatDock : Rewrote the script that was deleted

---
+++ structure/simple/BoatDock.hx
@@ -0,0 +1,21 @@
+function buildMainRooms(){
+    Struct.createSpecificRoom("BridgeEntrance").setName("start").addFlag(RoomFlag.Outside)
+        .chain(Struct.createSpecificRoom("BridgeBoatDock").addFlag(RoomFlag.Outside));
+}
+
+function buildMobRoster(_mobList){
+}
+
+function addTeleports(){
+}
+
+
+function setLevelInfo(_levelInfo){
+    setLevelInfoFrom("BoatDock", _levelInfo); 
+}
+
+
+function SetLevelProps(_levelProps){
+    setLevelPropsFrom("BoatDock", _levelProps);
+}
+
