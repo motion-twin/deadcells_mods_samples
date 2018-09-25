fe7a70d908dc736b28b551c14ba2a8332b9ba64d
crautou <crautou@motion-twin.fr>
2018-09-25 12:05:19 +0200

    Added structure for Throne level

---
+++ structure/simple/Throne.hx
@@ -0,0 +1,19 @@
+function buildMainRooms(){
+    Struct.createSpecificRoom("BossKingsHandEntrance").addFlag(RoomFlag.Outside).setName("start")
+        .chain(Struct.createSpecificRoom("BossKingsHandArena").addFlag(RoomFlag.Outside).setName("boss"))
+        .chain(Struct.createSpecificRoom("BossKingsHandThrone").addFlag(RoomFlag.Outside));
+}
+
+function buildMobRoster(_mobList){
+}
+
+function addTeleports(){
+}
+
+function setLevelInfo(_levelInfo){
+    setLevelInfoFrom("Throne", _levelInfo); 
+}
+
+function SetLevelProps(_levelProps){
+    setLevelPropsFrom("Throne", _levelProps);
+}
